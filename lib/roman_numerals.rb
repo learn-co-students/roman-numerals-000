74fc472ccf890e78f5748f714eab58a1ff7d1066
allensoberano <allensoberano@gmail.com>
2015-09-19 05:53:14 -0700

    done

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,41 +1,48 @@
 # Write your code here.
-require 'pry'
 
 class Integer
 
-  def to_roman()
-    number_to_convert = self
+  def to_roman
+    current_num = self.to_i
     roman_hash = {
                   1 => "I",
-                  2 => "II",
-                  3 => "III",
                   4 => "IV",
                   5 => "V",
-                  6 => "VI",
-                  7 => "VII",
-                  8 => "VII",
                   9 => "IX",
                   10 => "X",
+                  40 => "XL",
                   50 => "L",
+                  90 => "XC",
                   100 => "C",
+                  400 => "CD",
                   500 => "D",
+                  900 => "CM",
                   1000 => "M"
-    }
-    number_array = []
-    binding.pry
-    number_to_convert = number_to_convert.split("")
+      }
 
-    number_to_convert.each do |num|
-      number_array << num
+    result = ""
+    last_key = 0
+    last_value = 0
+    
+    while current_num > 0
+
+      #go through until key is greater than current number
+      roman_hash.each do |k, v|
+        if current_num >= k #finding which number until biggest
+          last_key = k
+          last_value = v #will become largest decimal when we find one bigger
+        end
+      end
+
+      current_num -= last_key.to_i
+      result << last_value
+     
     end
     
-
-    roman_hash[number_to_convert]
+    result
+    
   end
-
-
-
 end
 
-# a = 1
-# puts a.to_roman
+# a = 2015
+# a.to_roman
