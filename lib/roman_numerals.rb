7b4bccc30504b8e1616249a781411380d1300e20
NoneOfMaster <rcraigt@gmail.com>
2015-10-24 16:34:45 -0700

    funny roman pun

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require 'pry'
+
+class Integer
+
+  ROMANS = {
+    1000 => "M",
+    900 => "CM", 
+    500 => "D",
+    400 => "CD", 
+    100 => "C",
+    90 => "XC", 
+    50 => "L",
+    40 => "XL", 
+    10 => "X",
+    9 => "IX", 
+    5 => "V",
+    4 => "IV", 
+    1 => "I"
+  }
+
+  def to_roman
+    total = self
+    roman_numeral = ""
+    ROMANS.each{|number, numeral|
+      (total/number).times do roman_numeral << numeral end
+      total = total % number
+    }
+    roman_numeral
+  end
+
+end
+
+###### the trick here is that whenever total is devided by a greater number the result is zero, an integer not a float!!!!!
